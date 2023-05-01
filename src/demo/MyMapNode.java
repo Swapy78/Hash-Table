3070869a5dcb0ce83786df30d7da27328ef990ae
Swapy78 <swapnilbabanmane@gmail.com>
2023-05-01 22:58:05 +0530

    Initial commit

---
+++ src/demo/MyMapNode.java
@@ -0,0 +1,14 @@
+//Java program to create MyMapNode with Key Value pair
+
+package demo;
+
+class MyMapNode {
+    String key; // key for the map node
+    int value; // value for the map node
+    
+    // constructor to create a new map node with the given key and value
+    public MyMapNode(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
